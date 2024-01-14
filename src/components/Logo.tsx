045e3472efb9f2948cc6f9c1eb3e865022a9ffdc
hossam sulleman <93336567+hossam1231@users.noreply.github.com>
2024-01-14 22:05:10 +0000

    push

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,10 +1,6 @@
 import Image from 'next/image'
+import logo from '@/Images/logo.png'
 
-export function Logo(props: React.ComponentPropsWithoutRef<'svg'>) {
-  return (
-    <Image
-      src="https://firebasestorage.googleapis.com/v0/b/fir-d6285.appspot.com/o/Storefront%2012%20Storefront%20orange%20%26%20white.png?alt=media&token=a5c06e3e-05db-4685-b66e-eb81dcecd76d"
-      alt=""
-    />
-  )
+export function Logo() {
+  return <Image src={logo} alt="" width={50} height={50} unoptimized />
 }
