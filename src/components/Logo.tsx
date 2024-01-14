a65a01651e1f9c83d35f6a66cd74c1afd40a0b77
hossam sulleman <93336567+hossam1231@users.noreply.github.com>
2024-01-14 21:54:36 +0000

    push

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,10 +2,9 @@ import Image from 'next/image'
 
 export function Logo(props: React.ComponentPropsWithoutRef<'svg'>) {
   return (
-    <img
+    <Image
       src="https://firebasestorage.googleapis.com/v0/b/fir-d6285.appspot.com/o/Storefront%2012%20Storefront%20orange%20%26%20white.png?alt=media&token=a5c06e3e-05db-4685-b66e-eb81dcecd76d"
       alt=""
-      {...props}
     />
   )
 }
